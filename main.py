945b254679f21270721cbdd45e5705d8555e4694
ega <everette_allen@ncsu.edu>
2021-08-26 18:42:01 -0400

    Initial Comit
    Google Cloud function that looks up last word on the end of a calling url in a Google Sheet, gets a new URL and returns a 302 redirect with that URL

---
+++ main.py
@@ -0,0 +1,31 @@
+def go_url(request):
+    from flask import redirect, request
+    import urllib.request
+    import os
+
+    request_string = request.path.strip("/")
+    
+    # Here we get our GSheet id and columns to use from the function runtime variables
+
+    searchCol = os.environ.get('searchCol')
+    dataCol = os.environ.get('dataCol')
+    sheetID = os.environ.get('sheetID')
+    
+    # searchCol = "A"    # usually A
+    # dataCol = "B"     # usually B
+    # sheetID = "1y9Kbi2rCCzEgdj9Cs5pf914g2paS1jI6u9X6cpUEfbY"
+
+    # query the google sheet to get the url from the patch title  
+    # Example: column A has keyword to lookup and column B has URL to redirect to
+    sheetLink = "https://docs.google.com/spreadsheets/d/" + sheetID + "/gviz/tq?tqx=out:csv&tq=%20select%20" + dataCol + "%20WHERE%20" + searchCol + "=%27" + request_string + "%27"
+
+    try:
+        lstring = urllib.request.urlopen(sheetLink).read().decode('utf-8').strip('\"')
+        
+    except:
+        return "<h1 style='color: red;'> ERROR - Can not get link for " + f'{request_string}' + " from sheet </h1>"
+    
+    if not lstring.strip():
+        return "<h1 style='color: red;'> Link for keyword " +  f'{request_string}' + " not found </h1>"
+        
+    return redirect(lstring, code = 302)
